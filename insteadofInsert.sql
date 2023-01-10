69b69b408305e5f908f274476dc647ed43cc1591
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2023-01-10 13:03:39 +0530

    Create insteadofInsert.sql

---
+++ insteadofInsert.sql
@@ -0,0 +1,49 @@
+CREATE TABLE startup_audit 
+(
+  Event_type  VARCHAR2(15),
+  event_date  DATE,
+  event_time  VARCHAR2(15)
+);
+
+-- trigger 
+
+create or replace trigger tr_st_sudit 
+after startup on database
+begin
+ insert into startup_audit values (
+     ora_sysevent,
+     sysdate ,
+     to_char(sysdate,'hh24:mm:ss')
+ );
+ end;
+ /
+
+  -- create instead of insert trigger
+  create table  student( 
+      name varchar(15)
+  );
+ 
+ create table subject (
+     subname varchar(15)
+ );
+
+ insert into student values('charan');
+ insert into subject values('java');
+
+ create view vw_names as 
+ select name , subname from student , subject ;
+
+ insert into vw_names values('karan' , 'sql');
+
+ --creating a instead of insert trigger
+ create or replace trigger tr_name
+ instead of insert on vw_names
+ for each row
+ begin
+ insert into student(name) values (:new.name);
+ insert into subject(subname) values (:new.subname);
+ end;
+ /
+
+ select * from student;
+ select * from subject;
